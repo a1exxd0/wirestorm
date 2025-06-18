cd23d10488c9fa014b74ae6796a26a2ed8d3917b
Alex Do <alexxdo2004@gmail.com>
2025-06-18 22:03:56 +0100

    squash

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,18 +1,16 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use crate::{Ctmp, Error};
+use crate::{ctmp::Ctmp, Error};
 
 const BUFFER_SIZE: usize = 0x400;
 
-/// Each message is 64KB. 1000 messages is 64MB, trivial for
-/// even most embedded systems.
-///
-/// Meant for a single producer, single consumer mechanism
 pub struct MessageMgr {
     messages: [Ctmp; BUFFER_SIZE],
     read_pos: AtomicUsize,
     write_pos: AtomicUsize,
 }
+
+unsafe impl Sync for MessageMgr {}
 
 #[allow(dead_code)]
 impl MessageMgr {
@@ -38,5 +36,33 @@ impl MessageMgr {
 
         self.write_pos.store(next_write, Ordering::Release);
         Ok(())
+    }
+
+    /// needs to return a vector of owned msgs because we need to copy out messages 
+    /// before it gets overwritten by producer
+    /// 
+    /// this is batched since we require clones and allocs, the other is cheap
+    pub fn read_batch(&self, max_batch: usize) -> Vec<Ctmp> {
+        let mut read_idx = self.read_pos.load(Ordering::Acquire);
+        let write_idx = self.write_pos.load(Ordering::Acquire);
+
+        let msg_diff = if write_idx >= read_idx {
+            write_idx - read_idx
+        } else {
+            BUFFER_SIZE - read_idx + write_idx
+        };
+
+        let batch_size = msg_diff.min(max_batch);
+        let mut res = Vec::with_capacity(batch_size);
+
+        while read_idx != write_idx {
+            let slot = unsafe { &*self.messages.as_ptr().add(read_idx) };
+            res.push(slot.clone());
+
+            read_idx = (read_idx + 1) % BUFFER_SIZE;
+        }
+
+        self.read_pos.store(write_idx, Ordering::Release);
+        res
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,11 @@
 use thiserror::Error;
 
 mod buffer;
+mod ctmp;
 mod listener;
-
-const MAX_MSG_LEN: usize = 0xFFFF;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("exceeded write ptr beyond read")]
     BufferWriteOverflow,
-}
-
-/// The protocol specifies that each message contains
-/// a static magic byte, the data length, and the data
-/// itself.
-///
-/// A message is deemed invalid if the data inside of the message
-/// does not match header length specified
-#[derive(Clone, PartialEq)]
-pub struct Ctmp {
-    length: u16,
-    pub data: [u8; MAX_MSG_LEN],
-}
-
-impl Ctmp {
-    #[inline(always)]
-    pub fn new(valid_len: u16, data: [u8; MAX_MSG_LEN]) -> Self {
-        Ctmp {
-            length: valid_len,
-            data,
-        }
-    }
-}
-
-impl Default for Ctmp {
-    fn default() -> Self {
-        Ctmp {
-            length: 0,
-            data: [0; MAX_MSG_LEN],
-        }
-    }
 }
